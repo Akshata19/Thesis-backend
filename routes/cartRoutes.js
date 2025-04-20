86e46913923fca0e587016f536d7e1bf11b06bad
Akshata19 <akshatamithari@gmail.com>
2025-04-20 18:50:58 +0200

    changed Cart to cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const Cart = require('../models/Cart');
+const Cart = require('../models/cart');
 // Add to cart: POST /api/cart/add
 // POST /api/cart/add
 router.post('/add', async (req, res) => {
